c96f58e5c10c3eabdbb0ce1ed4ea5a47eaab7b41
Tomasz Rydza <me.try.dev@gmail.com>
2020-12-30 16:28:10 +0100

    Add StateMachine, PlatformerCharacter

--- src/Modules/Core/SceneController.gd
+++ src/Modules/Core/SceneController.gd
@@ -3,11 +3,10 @@ extends Node
 signal scene_started_loading(scene)
 signal scene_finished_loading(scene)
 
-const SCENE_LOAD_DELAY = 2.0
-
 func load_scene(scene):
-	if scene:
-		emit_signal("scene_started_loading", scene)
-		yield(get_tree().create_timer(SCENE_LOAD_DELAY), "timeout")
-		get_tree().change_scene_to(scene)
-		emit_signal("scene_finished_loading", scene)
+	emit_signal("scene_started_loading", scene)
+	get_tree().change_scene_to(scene)
+	emit_signal("scene_finished_loading", scene)
+
+func reload_current():
+	get_tree().reload_current_scene()
--- src/Modules/Physics/PhysicalObject2D.gd
+++ src/Modules/Physics/PhysicalObject2D.gd
@@ -2,28 +2,42 @@ extends KinematicBody2D
 
 class_name PhysicalObject2D
 
+# Properties
 export(float, 0.01, 100.0) var mass := 1.0
 export(float, 0.0, 1.0) var friction_coeff := 0.02
 export(float, 0.0, 1.0) var bounciness := 0.90
 
+# Constraints
+export(float, 0.0, 10000.0) var max_speed := 1000.0
+export(float, 0.0, 10000.0) var max_force := 1000.0
+export(bool) var should_rotate := false
+
 var velocity := Vector2()
 var acceleration := Vector2()
 
+const FLOOR_NORMAL = Vector2.UP
+const ROTATION_MIN_VELOCITY_SQ = pow(10.0, 2.0)
+
 func apply_force(force: Vector2) -> void:
-	acceleration += force / mass
+	acceleration += force.clamped(max_force) / mass
 
 func update() -> void:
 	var last_velocity = velocity
-	# apply friction
-	var friction = -1 * last_velocity * friction_coeff
-	apply_force(friction)
-	# apply acceleration
-	last_velocity += acceleration;
-	velocity = move_and_slide(last_velocity);
-	# bounce if collided
+	# Apply friction
+	if friction_coeff > 0.0:
+		var friction = -1 * last_velocity * friction_coeff
+		apply_force(friction)
+	# Apply acceleration
+	last_velocity += acceleration
+	last_velocity = last_velocity.clamped(max_speed)
+	velocity = move_and_slide(last_velocity, FLOOR_NORMAL)
+	# Bounce if collided
 	var slide_count = get_slide_count()
-	if slide_count:
+	if slide_count and bounciness:
 		var collision = get_slide_collision(slide_count - 1)
 		velocity = bounciness * last_velocity.bounce(collision.normal)
-	# reset acceleration
+	# Rotate if rotation enabled
+	if should_rotate and velocity.length_squared() >= ROTATION_MIN_VELOCITY_SQ:
+		rotation = lerp(rotation, velocity.angle(), 0.2) #look_at(global_position + velocity)
+	# Reset acceleration
 	acceleration = Vector2()
